fc706a74d255750099566197505fd66a4131034c
isucon <isucon@example.com>
2020-09-12 09:07:24 +0000

    chairにindex追加

--- mysql/db/0_Schema.sql
+++ mysql/db/0_Schema.sql
@@ -37,5 +37,7 @@ CREATE TABLE isuumo.chair
 	    kind        VARCHAR(64)     NOT NULL,
 	    popularity  INTEGER         NOT NULL,
 	    stock       INTEGER         NOT NULL,
-        INDEX chair_price (price)
+	    INDEX chair_price (price),
+	    INDEX chair_kind_stock (kind, stock),
+	    INDEX chair_popularity (popularity)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
