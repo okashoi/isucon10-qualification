ec657395bb0722c97bde36a35981a507ec9f1f27
yamachu <yamachu.dev@gmail.com>
2020-09-12 14:30:30 +0900

    これでデータ流せた

--- mysql/db/init.docker.sh
+++ mysql/db/init.docker.sh
@@ -2,12 +2,12 @@
 set -xe
 set -o pipefail
 
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
-export MYSQL_USER=${MYSQL_USER:-isucon}
-export MYSQL_DBNAME=${MYSQL_DBNAME:-isuumo}
-export MYSQL_PWD=${MYSQL_PASS:-isucon}
+exportMYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+MYSQL_PORT=${MYSQL_PORT:-3306}
+MYSQL_USER=${MYSQL_USER:-isucon}
+MYSQL_DBNAME=${MYSQL_DBNAME:-isuumo}
+MYSQL_PWD=${MYSQL_PASS:-isucon}
 export LANG="C.UTF-8"
 cd /opt/dbseed/
 
-cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql | mysql --defaults-file=/dev/null -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME -pisucon
