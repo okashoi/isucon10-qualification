710193ceeeaeb93f1a36e51ab8a8370e62dcc299
isucon <isucon@example.com>
2020-09-12 07:19:20 +0000

    colltion を直接指定

--- mysql/db/0_Schema.sql
+++ mysql/db/0_Schema.sql
@@ -1,38 +1,39 @@
 DROP DATABASE IF EXISTS isuumo;
-CREATE DATABASE isuumo;
+CREATE DATABASE isuumo COLLATE utf8mb4_general_ci;
+
 
 DROP TABLE IF EXISTS isuumo.estate;
 DROP TABLE IF EXISTS isuumo.chair;
 
 CREATE TABLE isuumo.estate
 (
-    id          INTEGER             NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)         NOT NULL,
-    description VARCHAR(4096)       NOT NULL,
-    thumbnail   VARCHAR(128)        NOT NULL,
-    address     VARCHAR(128)        NOT NULL,
-    latitude    DOUBLE PRECISION    NOT NULL,
-    longitude   DOUBLE PRECISION    NOT NULL,
-    rent        INTEGER             NOT NULL,
-    door_height INTEGER             NOT NULL,
-    door_width  INTEGER             NOT NULL,
-    features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
-);
+	    id          INTEGER             NOT NULL PRIMARY KEY,
+	    name        VARCHAR(64)         NOT NULL,
+	    description VARCHAR(4096)       NOT NULL,
+	    thumbnail   VARCHAR(128)        NOT NULL,
+	    address     VARCHAR(128)        NOT NULL,
+	    latitude    DOUBLE PRECISION    NOT NULL,
+	    longitude   DOUBLE PRECISION    NOT NULL,
+	    rent        INTEGER             NOT NULL,
+	    door_height INTEGER             NOT NULL,
+	    door_width  INTEGER             NOT NULL,
+	    features    VARCHAR(64)         NOT NULL,
+	    popularity  INTEGER             NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 CREATE TABLE isuumo.chair
 (
-    id          INTEGER         NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)     NOT NULL,
-    description VARCHAR(4096)   NOT NULL,
-    thumbnail   VARCHAR(128)    NOT NULL,
-    price       INTEGER         NOT NULL,
-    height      INTEGER         NOT NULL,
-    width       INTEGER         NOT NULL,
-    depth       INTEGER         NOT NULL,
-    color       VARCHAR(64)     NOT NULL,
-    features    VARCHAR(64)     NOT NULL,
-    kind        VARCHAR(64)     NOT NULL,
-    popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
-);
+	    id          INTEGER         NOT NULL PRIMARY KEY,
+	    name        VARCHAR(64)     NOT NULL,
+	    description VARCHAR(4096)   NOT NULL,
+	    thumbnail   VARCHAR(128)    NOT NULL,
+	    price       INTEGER         NOT NULL,
+	    height      INTEGER         NOT NULL,
+	    width       INTEGER         NOT NULL,
+	    depth       INTEGER         NOT NULL,
+	    color       VARCHAR(64)     NOT NULL,
+	    features    VARCHAR(64)     NOT NULL,
+	    kind        VARCHAR(64)     NOT NULL,
+	    popularity  INTEGER         NOT NULL,
+	    stock       INTEGER         NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
